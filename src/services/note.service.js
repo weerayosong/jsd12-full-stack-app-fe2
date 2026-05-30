4ecbe26c1825708699cb5529c420606270964f0e
weerayosong <weerayosong@outlook.com>
2026-05-30 21:26:02 +0700

    setup and prep before deployment

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3002/api/v2/notes";
+const API_URL = `${import.meta.env.VITE_API_URL || "http://localhost:3002/api/v2"}/notes`;
 
 export const NoteService = {
     getAll: async () => {
